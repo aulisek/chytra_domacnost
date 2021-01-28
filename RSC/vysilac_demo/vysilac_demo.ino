50806099ba293c9e94d91ae07b43330d4809c934
aulisek <aulichvlad@gmail.com>
2021-01-28 19:27:48 +0100

    jednoduchy vysil a prijimac

---
+++ RSC/vysilac_demo/vysilac_demo.ino
@@ -0,0 +1,39 @@
+/*
+  Example for different sending methods
+  
+  https://github.com/sui77/rc-switch/
+  
+*/
+
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  // Transmitter is connected to Arduino Pin #10  
+  mySwitch.enableTransmit(23);
+  
+  // Optional set protocol (default is 1, will work for most outlets)
+  // mySwitch.setProtocol(2);
+
+  // Optional set pulse length.
+  // mySwitch.setPulseLength(320);
+  
+  // Optional set number of transmission repetitions.
+  mySwitch.setRepeatTransmit(5);
+  
+}
+
+void loop() {
+
+
+  /* Same switch as above, but using decimal code */
+  mySwitch.send(1361, 24);
+  delay(10000);  
+  mySwitch.send(1364, 24);
+  delay(10000);  
+
+}
