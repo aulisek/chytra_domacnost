b542f404ffbbcd6b467c9d3e452dd5010358cb22
aulisek <aulichvlad@gmail.com>
2021-02-02 10:48:06 +0100

    pridan HTML kod, prace s knihovnou AsyncWebServer

---
+++ RSC/vysilac_filesystem/vysilac_filesystem.ino
@@ -0,0 +1,90 @@
+#include <RCSwitch.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+
+const char* ssid = "DOMA"; //Napište SSID
+const char* password = "VladaVlada12345"; //Heslo sítě
+
+const char* PARAM_MESSAGE = "message";
+
+AsyncWebServer server(80);
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println();
+
+//připojení souborového svazku
+if(!SPIFFS.begin()){
+  Serial.println("An Error has occurred while mounting SPIFFS");
+  return;
+}
+
+  Serial.print("Připojování k:");
+  Serial.println(ssid);
+
+  // Zahájení připojení k Wi-Fi
+  WiFi.begin(ssid, password);
+
+  Serial.println();
+  Serial.print("Připojování");
+
+  while( WiFi.status() != WL_CONNECTED ){
+      delay(500);
+      Serial.print(".");  
+  }
+
+  Serial.println();
+
+  Serial.println("Úspěšně připojeno k WIFI");
+  Serial.println("Vaše IP adresa: ");
+  Serial.println(WiFi.localIP() );
+
+server.on("/html", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/test_file.html", "text/html");
+});
+
+  server.begin();
+  Serial.println("Server je spuštěn");
+  
+  
+  // Vysílač připojen k pinu #23, výchozí protokol 1
+  mySwitch.enableTransmit(23);
+
+  // Nastavení počtu opakování vysílání
+  mySwitch.setRepeatTransmit(15);
+
+  // Send a GET request to <IP>/get?message=<message>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String message;
+      if (request->hasParam(PARAM_MESSAGE)) {
+        message = request->getParam(PARAM_MESSAGE)->value();
+        mySwitch.send(1361, 24);
+      } 
+      else {
+        message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, GET: " + message);
+      });
+
+/*
+   server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String message;
+      if (request->hasParam(vypnuto)) {
+        message = request->getParam(pokoj=vypnuto)->value();
+        mySwitch.send(1364, 24);
+      }
+      else {
+        message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, GET: " + message);
+  }
+*/ 
+
+}
+
+void loop() {
+}
