62fc179c935d08a2a66ae1ff45d0981e3c1f62d8
aulisek <aulichvlad@gmail.com>
2021-03-12 23:48:09 +0100

    přidáno detekce tmy přes fotorezistor

--- SRC/prijimac/prijimac.ino
+++ SRC/prijimac/prijimac.ino
@@ -4,15 +4,18 @@
 #define RELEpin 4          // pin připojené relé
 #define PIRpin 5           // pin připojený PIR senzor
 #define TLACITKOpin 3      // pin s tlačítkem (musí být #2/#3 kvůli přerušení)
+#define FOTOREZISTORpin A0 // pin s fotorezistorem, analogový!    
 
 //"debouncing" po stisku tlačítka
 #define PRODLEVA 50        // prodleva pro čtení tlačítka v milisekundách
 
-int PIRstav = LOW;         // inicializujeme počáteční stav jako vypnuto
-int hodnota = 0;           // proměná pro čtení hodnoty na senzoru
+int PIRstav = LOW;         // inicializujeme počáteční stav jako vypnuto (načítá stav PIR senzoru)
+int hodnota = 0;           // proměná pro čtení hodnoty na senzoru PIR
 
 int soucasny_stav = LOW;   // současný stav (mód ve kterém pracujeme) čidlo/(zap/vyp)
 
+int fotorezistor_hodnota = 0;
+int tma = 5;               // Napište, při jaké hodnotě na fotorezistoru chcete zapínat PIR (tma)
 // Inicializujeme, jestli je aktivní čtení z čidla pohybu PIR (trvale zapnuto = true)
 volatile boolean blokovani = false;       
 
@@ -42,6 +45,7 @@ void setup() {
   pinMode(RELEpin, OUTPUT);   // Nastavení relé jako výstup
   pinMode(PIRpin, INPUT);     // Nastavení PIR senzoru jako výstup
   pinMode(TLACITKOpin, INPUT);// Nastavení tlačítka jako vstup (zapojeno přes pull-down rezistor -> log1 = zapnuto)
+  pinMode(FOTOREZISTORpin, INPUT);
   
   mySwitch.enableReceive(0);  // Přijímač je na pinu přerušení 0 -> arduino uno pin #2
 
@@ -50,6 +54,10 @@ void setup() {
 }
 
 void loop() {
+  // Načtení napětí na vstupu s fotorezistorem
+  // Přemapování na menší rozsah z důvodu ustálení "rovnováhy"
+  fotorezistor_hodnota = map(analogRead(FOTOREZISTORpin), 0, 1023, 0, 100);
+  //Serial.println(fotorezistor_hodnota); // Zahltí konsoli, jen při debug
   if (digitalRead(TLACITKOpin) == HIGH) {
     if (soucasny_stav == LOW && (millis() - cas_zmeny) > PRODLEVA) {
       soucasny_stav = HIGH;
@@ -89,12 +97,13 @@ void loop() {
 
 // Pracuje pouze pokud nebylo stisknuto tlačíko nebo příkaz po rádiu
 // Zajišťuje zapíná na základě pohybu (PIR senzor)
-  if (!blokovani && (analogRead(A0) < 150)) {
+  if (!blokovani) {
     hodnota = digitalRead(PIRpin);  // Přečte výstup PIR senzoru
     // Kontrola, jestli je detekován pohyb
-    if (hodnota == HIGH) {
+    // Zároveň musí být splněna podmínka tmy (detekováno fotorezistorem)
+    if (hodnota == HIGH && (fotorezistor_hodnota <= tma)) {
       digitalWrite(RELEpin, LOW);  // zapne relé
-      
+      //Mění stav čidla v proměnné PIRstav z LOW na HIGH
       if (PIRstav == LOW) {
        Serial.println("Pohyb detekován");
         // Přepisujeme pouze stav pinu, nikoliv soucasny_stav (blokovani...)
@@ -102,7 +111,8 @@ void loop() {
       }
     }
     else {
-      digitalWrite(RELEpin, HIGH);
+      digitalWrite(RELEpin, HIGH); // Vypne relé
+      // Změní stav proměnné PIRstav na LOW
       if (PIRstav == HIGH) {
         Serial.println("Pohyb skončil");
         // Přepisujeme pouze stav pinu, nikoliv soucasny_stav (blokovani...)
